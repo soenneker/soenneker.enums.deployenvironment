c6451f12cae357d6be1bcd8f8ec55be1f7cfa9d3
Jake Soenneker <jake@soenneker.com>
2023-03-21 20:08:57 -0500

    Initial commit

---
+++ src/DeployEnvironment.cs
@@ -0,0 +1,39 @@
+﻿using Ardalis.SmartEnum;
+
+namespace Soenneker.Enums.DeployEnvironment;
+
+public sealed class DeployEnvironment : SmartEnum<DeployEnvironment>
+{
+    /// <summary>
+    /// PR and Local Unit and Integration Tests
+    /// </summary>
+    public static readonly DeployEnvironment Test = new(nameof(Test), 0);
+
+    /// <summary>
+    /// Developer local machine, pull request pipelines. <para/>
+    /// This coordinates with appsettings.json
+    /// </summary>
+    public static readonly DeployEnvironment Local = new(nameof(Local), 1);
+
+    /// <summary>
+    /// Azure Development <para/>
+    /// This coordinates with appsettings.Development.json
+    /// </summary>
+    public static readonly DeployEnvironment Development = new(nameof(Development), 2);
+
+    /// <summary>
+    /// Azure Staging <para/>
+    /// This coordinates with appsettings.Staging.json
+    /// </summary>
+    public static readonly DeployEnvironment Staging = new(nameof(Staging), 3);
+
+    /// <summary>
+    /// Azure Production <para/>
+    /// This coordinates with appsettings.Production.json
+    /// </summary>
+    public static readonly DeployEnvironment Production = new(nameof(Production), 4);
+
+    private DeployEnvironment(string name, int value) : base(name, value)
+    {
+    }
+}
