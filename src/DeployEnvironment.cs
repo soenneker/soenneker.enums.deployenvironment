fd4cf75c4024d271dfcb17746a6ed29c25af5e39
Jake Soenneker <jake@soenneker.com>
2025-02-22 13:21:41 -0600

    SmartEnum -> Intellenum

--- src/DeployEnvironment.cs
+++ src/DeployEnvironment.cs
@@ -1,39 +1,36 @@
-﻿using Soenneker.SmartEnum.Named;
+﻿using Intellenum;
 
 namespace Soenneker.Enums.DeployEnvironment;
 
-public sealed class DeployEnvironment : NamedSmartEnum<DeployEnvironment>
+[Intellenum<string>]
+public partial class DeployEnvironment
 {
     /// <summary>
     /// PR and Local Unit and Integration Tests
     /// </summary>
-    public static readonly DeployEnvironment Test = new(nameof(Test), 0);
+    public static readonly DeployEnvironment Test = new(nameof(Test));
 
     /// <summary>
     /// Developer local machine, pull request pipelines. <para/>
     /// This coordinates with appsettings.json
     /// </summary>
-    public static readonly DeployEnvironment Local = new(nameof(Local), 1);
+    public static readonly DeployEnvironment Local = new(nameof(Local));
 
     /// <summary>
     /// Azure Development <para/>
     /// This coordinates with appsettings.Development.json
     /// </summary>
-    public static readonly DeployEnvironment Development = new(nameof(Development), 2);
+    public static readonly DeployEnvironment Development = new(nameof(Development));
 
     /// <summary>
     /// Azure Staging <para/>
     /// This coordinates with appsettings.Staging.json
     /// </summary>
-    public static readonly DeployEnvironment Staging = new(nameof(Staging), 3);
+    public static readonly DeployEnvironment Staging = new(nameof(Staging));
 
     /// <summary>
     /// Azure Production <para/>
     /// This coordinates with appsettings.Production.json
     /// </summary>
-    public static readonly DeployEnvironment Production = new(nameof(Production), 4);
-
-    private DeployEnvironment(string name, int value) : base(name, value)
-    {
-    }
+    public static readonly DeployEnvironment Production = new(nameof(Production));
 }
